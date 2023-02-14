88114d5006f415148baafbdf06f5c8a65060ee53
Timothy <toqul4j@gmail.com>
2023-02-13 23:17:02 -0800

    3rd stage

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -12,20 +13,27 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
+}
 
-	defer conn.Close()
+func handleConnection(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
 
-	buf := make([]byte, 1024)
+		c.Write([]byte("+PONG\r\n"))
 
-	if _, err := conn.Read(buf); err != nil {
-		fmt.Println("error reading from client: ", err.Error())
-		os.Exit(1)
 	}
-
-	conn.Write([]byte("+PONG\r\n"))
 }
